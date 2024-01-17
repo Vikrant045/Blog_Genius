766064f10af5e6c843c2e6531fdbe4a2672d769b
Vikrant <vikrant602019@gmail.com>
2024-01-17 12:42:29 +0530

    Initial commit on main branch

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// Import the specific icon you need
+
+export default function Logo({ width = "300px"}) {
+  
+
+  return (
+    <div className="text-gray-100 text-xl font-bold">
+      Blog Genius
+    </div>
+  );
+}
